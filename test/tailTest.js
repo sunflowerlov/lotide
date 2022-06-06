4161f4c1b63b6ff0c5828b79befd31d539b4a1f6
Lisha Liang <lishaliang1999@gmail.com>
2022-06-06 23:33:20 +0000

    assert

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert; 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("return tail for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+});
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
