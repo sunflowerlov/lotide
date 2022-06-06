6802c09384f92e85428a603f7fedde21d99f989a
Lisha Liang <lishaliang1999@gmail.com>
2022-06-06 21:21:57 +0000

    require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,5 @@
 // FUNCTION IMPLEMENTATION
+const eqArrays = require('./eqArrays')
 const assertArraysEqual = function(array1, array2) {
 
   if (eqArrays(array1, array2)) {
@@ -12,17 +13,8 @@ const assertArraysEqual = function(array1, array2) {
 
 
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  for (let index = 0; index < arr1.length; index++) {
-    if (arr1[index] !== arr2[index]) {
-      return false
-  }
-  return true
-}
-}
 
 assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // => should PASS
 assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4, 6, 7, 2]);
+
+module.exports = assertArraysEqual;
