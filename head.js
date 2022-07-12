73340c5b2f80e1f58dfef94028cfa1bf3c7fd11b
Lisha Liang <lishaliang1999@gmail.com>
2022-07-12 21:17:25 +0000

    head with assert

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
@@ -8,15 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const head = function (input) {
   return input[0]
 }
 
-assertEqual(head([5,6,7]), 5);//5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");//"hello"
-assertEqual(head([]));
-assertEqual(head([2]), 2);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+module.exports = head;
