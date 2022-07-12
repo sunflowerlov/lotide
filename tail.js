a80cc7a78f64446e41c5ec1ab96154bc08d96a96
Lisha Liang <lishaliang1999@gmail.com>
2022-07-12 21:29:03 +0000

    tail with assert

--- tail.js
+++ tail.js
@@ -4,19 +4,17 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
   else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
- assertEqual(words.length, 3); // original array should still have 3 elements!
 const tail = function (words) {
   return words.slice(1);
 }
-tail(words);// no need to capture the return value since we are not checking it
-const output1 = tail([])
-tail([])
+// no need to capture the return value since we are not checking it
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
