2cefdc2b5b125068a9e88f60fc144ade9893eb42
Lisha Liang <lishaliang1999@gmail.com>
2022-07-20 16:15:57 +0000

    letter

--- letterPositions.js
+++ letterPositions.js
@@ -23,16 +23,18 @@ const eqArrays = function (arr1, arr2) {
 }
 
 const letterPositions = function(sentence) {
+  const noSpaces = sentence.split(" ").join('')
   const results = {};
-  for (const letter in sentence) {
-    if (!results[sentence[letter]]) {
-      results[sentence[letter]] = []
+  
+  for (let i = 0; i < noSpaces.length; i++) {
+    if (!results[noSpaces[i]]) {
+      results[noSpaces[i]] = []
     }
-    results[sentence[letter]].push(Number(letter));
+    results[noSpaces[i]].push(Number(i))
   }
   console.log(results)
   return results;
- 
 };
-console.log(letterPositions("hello").e)
-assertArraysEqual(letterPositions("hello").e, [1]);//.e means access the e key
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("lighthouse in the house").l, [0]);
