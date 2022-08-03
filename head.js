055b2d3b136e71eb26084aa305baca46b4662049
Lisha Liang <lishaliang1999@gmail.com>
2022-08-03 23:48:41 +0000

    head

--- head.js
+++ head.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-  else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 const head = function (input) {
   return input[0]
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
 module.exports = head;
