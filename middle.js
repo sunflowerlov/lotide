93d71d73224a4969c142704e8f997289b6ad2fba
Lisha Liang <lishaliang1999@gmail.com>
2022-08-04 00:20:32 +0000

    refractor

--- middle.js
+++ middle.js
@@ -1,51 +1,22 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+
+const middle = function (input){
+  let result = []
+  if (input.length <= 2) {
+    return result
+  }
+  else if (input.length % 2 !== 0){
+    result.push(input[Math.floor(input.length / 2 )])
+    console.log(result)
+    return result
   }
   else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-const eqArrays = function (arr1, arr2) { //arr1 and arr2 mean eqArrqys[0][1]
-  if (arr1.length !== arr2.length) { //check the lengths are the same
-    return false
-  }
-  for (let index = 0; index < arr1.length; index++) {//loop arr1
-    if (arr1[index] !== arr2[index]) { //check each arr1[i] and arr2[i] value are the same
-      return false
-  }
-  return true //if all correct return true
+    result.push(input[Math.floor((input.length - 1) / 2)], input[Math.floor((input.length - 1) / 2) + 1])
+    console.log(result)
+    return result
   }
 }
 
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true); 
 
-const middle = function(array) {
 
-  if (array.length <= 2) {
-    middleArr = [];
-  }
-  else if (array.length % 2 !== 0) {
-    middleArr = [array[Math.floor(array.length / 2 )]];
-  }
-  else if (array.length % 2 === 0) {
-    middleArr = [array[Math.floor((array.length - 1) / 2)], array[Math.floor((array.length - 1) / 2) + 1]];
-    
-  }
-  console.log (middleArr)
-}
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
